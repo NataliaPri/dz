9481ce7011bd710a1a31a4656379bdb130d842c8
NataliaPri <77389496+NataliaPri@users.noreply.github.com>
2021-03-23 19:44:30 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void BucketSort(int[] A)
+            {
+                int k = 0;
+                int n = A.Length;
+                int[][] B = new int[n][];
+
+                for (int i = 0; i < n; i++)
+                    //нужно вставить А[i] в масcив B
+                    for (int a = 0; a < n; a++)
+                        Array.Sort(B[a]);
+
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < B[i].Length; j++)
+                        A[k++] = B[i][j++];
+            }
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    { 
+            static void Main(int[] arr, int l, int r)
+            {
+                //i и j нужны для цикла
+                int i = l;
+                int j = r;
+                int x = arr[(l + r) / 2]; //Опорная
+                                          //Цикл сортировка
+                while (i <= j)
+                {
+                    //Деление на меньше и больше опорного
+                    while (arr[i] < x) i++;
+                    while (arr[j] > x) j--;
+                    //Если i<=j:
+                    if (i <= j)
+                    {
+                        //меняем значение элементов
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        i++;
+                        j--;
+                    }
+                }
+                //Рекурсия
+                if (l < j) Main(arr, l, j);
+                if (i < r) Main(arr, i, r);
+            }     
+    }
+}
